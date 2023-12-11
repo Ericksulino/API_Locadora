eb9443b6073d854888dee29a6f7f724a2065e97b
ericksulino <ericksulino@gmail.com>
2023-12-11 19:37:55 -0300

    CRUD secure

--- src/services/secure.service.js
+++ src/services/secure.service.js
@@ -10,7 +10,62 @@ const findAllService = async () => {
     }
 };
 
+const findByIdService = (id) => Secure.findById(id);
+
+const findByDescricaoService = async (descricao) => {
+    try {
+        const secure = await Secure.findOne({ descricao });
+        return secure;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+const findByCoberturaDanosService = async (tipoCobertura) => {
+    try {
+        const secureList = await Secure.find({ 'coberturaDanos.type': tipoCobertura });
+        return secureList;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+const updateService = async (secureId, updatedData) => {
+    
+    try {
+        const secure = await Secure.findByIdAndUpdate(secureId, updatedData, { new: true });
+
+        if (!secure) {
+            throw new Error('Seguro não encontrado.');
+        }
+
+        return secure;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+
+const eraseService = async (secureId) => {
+    try {
+        const secure = await Secure.findByIdAndDelete(secureId);
+
+        if (!secure) {
+            throw new Error('Seguro não encontrado.');
+        }
+
+        return secure;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
 module.exports = {
     createService,
-    findAllService
+    findAllService,
+    findByIdService,
+    findByDescricaoService,
+    findByCoberturaDanosService,
+    updateService,
+    eraseService
 };
